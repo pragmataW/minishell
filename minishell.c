24ada93259a754db9e70adbec808c6c36838ae9d
Yusuf Ciftci <yciftci@pop-os.localdomain>
2023-06-23 15:34:41 +0300

    unfinished yet

--- minishell.c
+++ minishell.c
@@ -1,18 +1,39 @@
 #include "minishell.h"
 
+void free_list(t_list **list)
+{
+	t_list	*iter;
+	t_list	*tmp;
+	
+	iter = *list;
+	while (iter)
+	{
+		tmp = iter->next;
+		free(iter->command);
+		free(iter);
+		iter = tmp;
+	}
+}
+
 int	main(int argc, char *argv[], char **env)
 {
 	char	*prompt;
 	t_list	**splited_str;
+	t_list	*iter;
+
+	prompt = readline("\033[1;31myciftci🥵minishell->\033[0m");
 	while (1)
 	{
-		prompt = readline("\033[1;31myciftci🥵minishell->\033[0m");
 		splited_str = lexer(prompt, 0, 0, 0);
-		printf("%s\n", (*splited_str)->command);
-		printf("%s\n", (*splited_str)->next->command);
-		printf("%s\n", (*splited_str)->next->next->command);
-		printf("%s\n", (*splited_str)->next->next->next->command);
-		printf("%s\n", (*splited_str)->next->next->next->next->command);
-		system("leaks a.out");
+		iter = *splited_str;
+		while (iter)
+		{
+			printf("%s\n", iter->command);
+			iter = iter->next;
+		}
+		free(prompt);
+		free(iter);
+		free_list(splited_str);
+		prompt = readline("\033[1;31myciftci🥵minishell->\033[0m");
 	}
 }
